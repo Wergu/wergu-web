9d3e1f6910a6dff3562f22340b6da3c3b8bbdc46
kheuch2020 <cheikh@mobility-sn.com>
2020-06-07 14:00:21 +0000

    removing lazy loading

--- src/Containers/LandingPage.js
+++ src/Containers/LandingPage.js
@@ -1,15 +1,16 @@
-import React, { useRef, Suspense } from 'react';
+import React, { useRef } from 'react';
 import img_mobile1 from "../img/img_mobile1.png"
 import img_mobile2 from "../img/img_mobile2.png"
 import img_mobile3 from "../img/img_mobile3.png"
 import img_mobile4 from "../img/img_mobile4.png"
 import animateScrollTo from "animated-scroll-to"
 
-const TopNav = React.lazy(() => import("../components/TopNav"))
-const Bande1 = React.lazy(() => import("../components/Bande1"))
-const WerguPharma = React.lazy(() => import("../components/WerguPharma"))
-const Team = React.lazy(() => import("../components/Team"))
-const Contact = React.lazy(() => import("../components/Contact"))
+import TopNav from '../components/TopNav';
+import Bande1 from '../components/Bande1';
+import WerguPharma from '../components/WerguPharma';
+import Team from '../components/Team';
+import Contact from '../components/Contact';
+
 
 const LandingPage = () => {
     const accueil = useRef(null)
@@ -36,7 +37,7 @@ const LandingPage = () => {
     }
     return (
         <>
-            <Suspense fallback="">
+
             <TopNav
                 handleNavClick={handleNavClick}
                 references={{
@@ -46,9 +47,6 @@ const LandingPage = () => {
                     nous_contacter
                 }}
             />
-            </Suspense>
-
-            <Suspense fallback="">
             <Bande1
                 mt={150}
                 references={{
@@ -59,47 +57,33 @@ const LandingPage = () => {
                 needStoreButtons
                 principalImage={img_mobile1}
             />
-            </Suspense>
-
             <p style={styles.sectionTitle} ref={nos_solutions}>Nos solutions</p>
-            <Suspense fallback="">
             <Bande1
                 text1="Trouvez la pharmacie ouverte la plus proche !"
                 text2="Ne vous déplacez plus à perte. En un clic, trouvez les informations sur les 5 pharmacies ouvertes les plus proches de vous. On vous indique aussi le chemin et la durée du trajet,  que vous soyez  en voiture ou à pied."
                 reversed
                 principalImage={img_mobile2}
             />
-            </Suspense>
             <div style={{ backgroundColor: "#F5F5F5", padding: 10 }}>
-            <Suspense fallback="">
                 <Bande1
                     text1="Faites vous livrer vos médicaments chez vous !"
                     text2="Plus de problème de monnaie ou de déplacement. Photographiez votre ordonance à travers l’appli, nos pharmaciens partenaires et nos livreurs s’occupent du reste. Suivez en temps réel votre commande."
                     principalImage={img_mobile3}
                 />
-            </Suspense>
             </div>
-            <Suspense fallback="">
+
             <Bande1
                 text1="Informez vous sur la disponibilité et le prix de vos médicaments !"
                 text2="Inutile de vous déplacer pour vous renseigner sur la disponibilité et le prix de vos médicaments. Avec wergu, la réponse se trouve au bout de vos doigts."
                 principalImage={img_mobile4}
                 reversed
             />
-            </Suspense>
 
-            <Suspense fallback="">
             <WerguPharma/>
-            </Suspense>
-
             <p style={styles.sectionTitle} id="ttt" ref={la_team}>La team</p>
-            <Suspense fallback="">
             <Team/>
-            </Suspense>
             <p style={styles.sectionTitle} ref={nous_contacter}>Contactez nous</p>
-            <Suspense fallback="">
             <Contact/>
-            </Suspense>
             
 
         </>
